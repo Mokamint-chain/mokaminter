0f8be0fc7cced83775520239da96c0c1711262f5
Fausto Spoto <fausto.spoto@mokamint.io>
2026-07-27 17:34:46 +0200

    updated default max nonces to 20000

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "io.mokamint.android.mokaminter"
         minSdk = 26
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
